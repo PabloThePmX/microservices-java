2aa6e8be8e9c8c670370e1698c6ac351e3d35e31
Pablo <pablosarturi1@gmail.com>
2025-04-11 21:47:56 -0300

    create and use config component

--- greeting-service/src/main/java/br/edu/atitus/greeting_service/GreetingController.java
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/GreetingController.java
@@ -8,15 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.greeting_service.configs.GreetingConfig;
+
 @RestController
 @RequestMapping("/greeting-service")
 public class GreetingController {
 	
+	/*
 	@Value("${greeting-service.greeting}")
 	private String greeting;
 	
 	@Value("${greeting-service.default-name}")
 	private String defaultName;
+	*/
+	
+	private final GreetingConfig greetingConfig;
+	
+	public GreetingController(GreetingConfig greetingConfig){
+		this.greetingConfig = greetingConfig;
+	}
 	
 	@GetMapping({"","/","/{namePath}"})
 	public ResponseEntity<String> getGreeting(@PathVariable(required = false) String namePath, @RequestParam(required = false) String name){
@@ -24,6 +34,6 @@ public class GreetingController {
 		
 		return name != null 
 				? ResponseEntity.ok(String.format("Hello Sir %s!", name)) 
-				: ResponseEntity.ok(String.format("%s %s!", greeting, defaultName));
+				: ResponseEntity.ok(String.format("%s %s!", greetingConfig.getGreeting(), greetingConfig.getDefaultName()));
 	} 
 }
