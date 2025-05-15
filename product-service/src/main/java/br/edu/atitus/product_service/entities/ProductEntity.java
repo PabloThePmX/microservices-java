0f5c7fb6e852476207cd82643fae192bc2e16abf
Pablo <pablosarturi1@gmail.com>
2025-05-15 01:28:41 -0300

    create product microservice

---
+++ product-service/src/main/java/br/edu/atitus/product_service/entities/ProductEntity.java
@@ -0,0 +1,115 @@
+package br.edu.atitus.product_service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "tb_product")
+public class ProductEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "description", length = 100, nullable = false)
+	private String description;
+	
+	@Column(name = "brand", length = 255, nullable = false)
+	private String brand;
+	
+	@Column(name = "model", length = 255, nullable = false)
+	private String model;
+	
+	@Column(name = "price", length = 53,  nullable = false)
+	private double price;
+	
+	@Column(name = "currency", length = 3, nullable = false)
+	private String currency;
+	
+	@Column(name = "stock", nullable = false)
+	private int stock;
+	
+	@Transient
+	private String environment;
+	
+	@Transient
+	private double convertedPrice;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	public double getConvertedPrice() {
+		return convertedPrice;
+	}
+
+	public void setConvertedPrice(double convertedPrice) {
+		this.convertedPrice = convertedPrice;
+	}
+	
+}
